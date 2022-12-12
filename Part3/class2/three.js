d87246cfe52a7d27d44e36854b111535b57ee602
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-12-12 11:17:39 +0530

    this

---
+++ Part3/class2/three.js
@@ -0,0 +1,14 @@
+class Test {
+    constructor() {
+        console.log("executing automatically")
+    }
+    get_Name() {
+        console.log("Test Case 123")
+    }
+}
+new Test()
+new Test()
+new Test()
+new Test()
+new Test()
+new Test()
