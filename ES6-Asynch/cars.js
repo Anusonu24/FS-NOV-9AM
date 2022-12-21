ad0a8d940ffab2f2a405217ae63b973009f4b23f
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-12-21 10:57:42 +0530

    okie

---
+++ ES6-Asynch/cars.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log(1)
+}, 4000);
+setTimeout(() => {
+    console.log(2)
+}, 2000);
+setTimeout(() => {
+    console.log(3)
+}, 1000);
+setTimeout(() => {
+    console.log(4)
+}, 8000);
+
